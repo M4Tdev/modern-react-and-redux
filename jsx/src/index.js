9fc3a7f5063ce411120987217ff94a19e7ebb090
M4T <mateo.lesiuk@gmail.com>
2019-03-16 19:59:27 +0100

    Done Section 2

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,12 +1,26 @@
+/* eslint-disable jsx-a11y/label-has-for */
+// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a react component
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = props => {
+  const buttonText = { text: 'Click Me!' };
+  const labelText = 'Enter name:';
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input type="text" id="name" />
+      <button type="button" style={{ backgroundColor: 'blue', color: 'white' }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
+};
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector('#root'));
